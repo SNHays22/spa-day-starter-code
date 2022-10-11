72b61fde7c5a017da811ceef6e00af9323bc93fd
SNHays22 <codergrl22@outlook.com>
2022-10-10 20:41:20 -0500

    validation studio

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,27 +3,29 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("add")
-    public String displayAddUserForm(){
+    public String displayAddUserForm(Model model){
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping("add")
-    public String processAddUser(Model model, @ModelAttribute User user, String verify){
-
-        if(user.getPassword().equals(verify)){
+    public String processAddUser(@ModelAttribute @Valid User user, Errors errors, Model model){
+        if(errors.hasErrors()){
+            return "user/add";
+        }
+        else {
             return "user/index";
         }
-        model.addAttribute("error", "Passwords do not match :(");
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        return "user/add";
 
 
     }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,18 +1,52 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class User {
 
+    @NotEmpty(message="Please enter a username")
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message="Must enter a valid email.")
     private String email;
+
+    @NotEmpty(message="Please enter a password.")
+    @Size(min=6,message="Password must be at least 6 characters")
     private String password;
 
+    @NotEmpty(message="Passwords do not match.")
+    private String verifyPassword;
 
-    public void UserProfile(String username, String email, String password) {
+
+    private void checkPassword(){
+        if(this.password != null && this.verifyPassword != null && !this.password.equals(this.verifyPassword)){
+            this.verifyPassword = null;
+        }
+    }
+
+
+    public User(){
+
+    }
+
+    public void UserProfile(String username, String email, String password, String verifyPassword) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -60,6 +94,7 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
 
 
